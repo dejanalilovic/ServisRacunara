a944575474f0fc334c113b20fb3958d9962ca791
Dejan <Dejan@korisnik-PC>
2018-11-16 22:59:59 +0100

    ServisRacunara

--- ServisRacunara/src/Racunar.java
+++ ServisRacunara/src/Racunar.java
@@ -1,4 +1,42 @@
 
 public class Racunar {
 
+	private String serijskiBroj;
+	private double brzinaProcesora;
+	private String markaRacunara;
+	
+	public Racunar(String serijskiBroj, double brzinaProcesora, String markaRacunara) {
+		super();
+		this.serijskiBroj = serijskiBroj;
+		this.brzinaProcesora = brzinaProcesora;
+		this.markaRacunara = markaRacunara;
+	}
+	
+	public String getSerijskiBroj() {
+		return serijskiBroj;
+	}
+
+	public void setSerijskiBroj(String serijskiBroj) {
+		this.serijskiBroj = serijskiBroj;
+	}
+
+	public double getBrzinaProcesora() {
+		return brzinaProcesora;
+	}
+
+	public void setBrzinaProcesora(double brzinaProcesora) {
+		this.brzinaProcesora = brzinaProcesora;
+	}
+
+	public String getMarkaRacunara() {
+		return markaRacunara;
+	}
+
+	public void setMarkaRacunara(String markaRacunara) {
+		this.markaRacunara = markaRacunara;
+	}
+	
+	
+	
+	
 }
