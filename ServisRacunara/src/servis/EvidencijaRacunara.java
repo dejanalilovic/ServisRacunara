ae07655ae573be57473328784051c8c2c539e732
Dejan <Dejan@korisnik-PC>
2018-11-17 13:05:45 +0100

    ServisRacunara

--- ServisRacunara/src/servis/EvidencijaRacunara.java
+++ ServisRacunara/src/servis/EvidencijaRacunara.java
@@ -1,5 +1,17 @@
 package servis;
 
+import java.time.LocalDate;
+
 public class EvidencijaRacunara {
 
+	private String vlasnik;
+	private String evidencioniBroj;
+	private LocalDate datumZavrsetkaServisa;
+	private LocalDate datumPrijemaRacunara;
+	
+	public void informacijeOEvidencijiRacunara() {
+		
+	}
+	
+	
 }
